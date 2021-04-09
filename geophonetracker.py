17d14d4f92c83775504c9f55c0691c8514a5baf8
krishpranav <krisna.pranav@gmail.com>
2021-04-09 08:35:40 -0400

    done changes

--- geophonetracker.py
+++ geophonetracker.py
@@ -37,3 +37,41 @@ parser.add_argument('-s', '--subdomain', help='Provide Subdomain for Serveo URL 
 parser.add_argument('-k', '--kml', help='Provide KML Filename ( Optional )')
 parser.add_argument('-t', '--tunnel', help='Specify Tunnel Mode [ Available : manual ]')
 parser.add_argument('-p', '--port', type=int, default=8080, help='Port for Web Server [ Default : 8080 ]')
+
+args = parser.parse_args()
+subdom = args.subdomain
+kml_fname = args.kml
+tunnel_mode = args.tunnel
+port = args.port
+
+row = []
+info = ''
+result = ''
+version = '1.2.5'
+
+def banner():
+	print (G +
+	r''' GEO PHONE TRACKER''' + W)
+	print('\n' + G + '[>]' + C + ' Created By : ' + W + 'krishpranav')
+	print(G + '[>]' + C + ' Version    : ' + W + version + '\n')
+
+
+def ver_check():
+    print(G + '[+]' + C + ' Checking for Updates.....', end='')
+    ver_url = 'https://raw.githubusercontent.com/krishpranav/geo-phonetracer/master/version.txt'
+    try:
+        ver_rqst = requests.get(ver_url)
+        ver_sc = ver_rqst.status_code
+        if ver_sec == 200:
+            github_ver = ver_rqst.text
+            github_ver = github_ver.strip()
+
+            if version == github_ver:
+                print(C + '[' + G + ' Up-To-Date ' + C +']' + '\n')
+            else:
+                print(C + '[' + G + ' Available : {} '.format(github_ver) + C + ']' + '\n')
+        else:
+            print(C + '[' + R + ' Status : {} '.format(ver_sc) + C + ']' + '\n')
+    except Exception as e:
+        print('\n' + R + '[-]' + C + ' Exception : ' + W + str(e))
+
